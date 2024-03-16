a61b418457aa2b7621c21285ba0d9da424b8aa19
Felipe Alves <ftelles.bus@gmail.com>
2024-03-16 15:17:58 -0300

    Rerun build

--- app/components/ImageEditor/__test__/useImageEditor.test.ts
+++ app/components/ImageEditor/__test__/useImageEditor.test.ts
@@ -1,13 +1,30 @@
-import { renderHook } from "@testing-library/react";
+import path from "path";
+import fs from "fs";
+import { renderHook, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { useImageEditor } from "..";
 import { act } from "react-dom/test-utils";
 
+const readFile = (filePath: string) => {
+  return new Promise<File>((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        reject();
+      }
+
+      // Create a File object
+      const file = new File([data], "file.jpg", { type: "image/jpg" });
+      resolve(file);
+    });
+  });
+};
+
 describe("useImageEditor", () => {
   const { result } = renderHook(() => useImageEditor());
 
-  it("Should toggle lockProportions", async () => {
+  it("Should toggle 'lock proportions'", async () => {
     expect(result.current.lockProportions).toBe(true);
 
     act(() => {
@@ -19,5 +36,14 @@ describe("useImageEditor", () => {
       result.current.toggleLockProportions();
     });
     expect(result.current.lockProportions).toBe(true);
+  });
+
+  it("Should handle image upload", async () => {
+    const file = await readFile(`${__dirname}/assets/girl.jpeg`);
+
+    await act(() => {
+      result.current.handleFileUpload([file]);
+    });
+    expect(result.current.imageDimensions).toBe({ width: 183, height: 275 });
   });
 });
