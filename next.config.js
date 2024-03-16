d347d943acd60ec203c7ce62b27ec5d4007561fb
Felipe Alves <ftelles.bus@gmail.com>
2024-03-16 15:31:40 -0300

    Rerun build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", distDir: "docs" };
+const nextConfig = {
+  output: "export",
+  distDir: "docs",
+  assetPrefix: "/photo-framer",
+};
 
 module.exports = nextConfig;
